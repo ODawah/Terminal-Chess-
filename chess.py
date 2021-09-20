b4e0c0d3a153f4bf180d2cf7c9965e56cdff1e8f
ODawah <omar.sanoval550@gmail.com>
2021-09-20 15:48:42 +0200

    fixed the pawn rule & added the rook rule

--- chess.py
+++ chess.py
@@ -10,21 +10,61 @@ def new_board():
         ["Br", "Bb", "Bn", "Bq", "Bk", "Bn", "Bb", "Br"],
     ]
 
+def pawn(player,x1,x2,y1,y2,board):
+    if(player == 1):
+        if ((y2-y1 == 2) and board[x2][y2] == "-":
+            return True
+        elif( (y2 - y1 == 1) and board[x2][y2] == "-"):
+            return True
+        else: 
+             print("pawn doesn't move like that")
+            return False
+
+    elif (player == 2):
+       if ((y2-y1 == -2) and board[x2][y2] == "-":
+            return True
+        elif( (y2 - y1 == -1) and board[x2][y2] == "-"):
+            return True
+        else: 
+             print("pawn doesn't move like that")
+            return False
+
+def rook(x1,x2,y1,y2,board):
+        for x in range (x1,x2):
+            if board[x][y2] != "-":
+                print("rook doesn't move like that")
+                return False
+            else:
+                return True
+
+
+
+
+
 
 def make_move(PLAYER, x1, y1, x2, y2):
     board = new_board()
     if(PLAYER == 1 and board[x1][y1][0]=="W"):
         if(board[x1][y1][1] == "p"):
-            if not ( (y2-y1 == 2) or (y2 - y1 == 1)):
-                print("pawn doesn't move like that")
+            if (pawn(PLAYER,x1,x2,y1,y2,board)):
+                board[x1][y1], board[x2][y2] = "-", board[x1][y1]
+            
+            elif(board[x1][y1] == "r"):
+                if(rook(PLAYER,x1,x2,y1,y2,board)):
+                board[x1][y1], board[x2][y2] = "-", board[x1][y1]
+                
 
     if(PLAYER == 2 and board[x1][y1][0]=="B"):
         if(board[x1][y1][1] == "p"):
-            if not ( (y2-y1 == -2) or (y2 - y1 == -1)):
-                print("pawn doesn't move like that")
+            if (pawn(PLAYER,x1,x2,y1,y2,board)):
+                board[x1][y1], board[x2][y2] = "-", board[x1][y1]
+        elif(board[x1][y1] == "r"):
+            if(rook(PLAYER,x1,x2,y1,y2,board)):
+                board[x1][y1], board[x2][y2] = "-", board[x1][y1]
+    else:
+        print('Play with your pieces only')
 
-    # moving on board
-    board[x1][y1], board[x2][y2] = "-", board[x1][y1]
+    
 
 
 def draw_board(board):
